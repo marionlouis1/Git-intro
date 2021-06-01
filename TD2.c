2879df50f8af35e1ae7f933152ead989d191455b
Louis Marion <marionlouis1@gmail.com>
2021-06-01 16:32:37 +0200

    fonction addone

--- TD2.c
+++ TD2.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include "main.h"
+
+#define PI 3.14
 
 int main() {
-    printf("Hello,\nJe suis Louis \n");
-    int b =5;
-    printf("b=%d\n",b);
+    printf(" addone(5) = %d\n", addone(5)); //UTILISATION
+    float a = PI;
+    printf(" pi = a=%d\n", a);
+}
+
+int addone(int x) { // DEFINITION
+    return x+1;
 }
