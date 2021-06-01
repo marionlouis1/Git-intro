de903c590631df69d14d350328f79f5ff2a2c143
Louis Marion <marionlouis1@gmail.com>
2021-06-01 16:42:20 +0200

    séparation des codes fonction

--- TD2.c
+++ TD2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include "module.h"
 
 #define PI 3.14
 
@@ -7,8 +7,4 @@ int main() {
     printf(" addone(5) = %d\n", addone(5)); //UTILISATION
     float a = PI;
     printf(" pi = a=%d\n",a);
-}
-
-int addone(int x) { // DEFINITION
-    return x+1;
 }
---
+++ module.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "module.h"
+
+
+int addone(int x) { // DEFINITION
+    return x+1;
