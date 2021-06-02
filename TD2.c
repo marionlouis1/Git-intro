14f1cd9d4835536b130bdd144c54afa3a7ef480c
Louis Marion <marionlouis1@gmail.com>
2021-06-02 11:13:57 +0200

    affichage d'une chaîne de charactères

--- TD2.c
+++ TD2.c
@@ -47,8 +47,8 @@ int main() {
     char seq[10] = "salutlegar";
     printf("la séquence est : %s\n", seq);
     printf("longueur de seq est : %d\n", mylen(seq));
-    */
-
+    
+    // Somme de 2 tableaux
     int n  = 10 ;
     int tab1[] = {1,2,3,4,5,6,7,8,9,10} ; 
     int tab2[] = {10,9,8,7,6,5,4,3,2,1} ;
@@ -57,5 +57,11 @@ int main() {
         sum_tab_i (tab1, tab2, n, i, tab_res) ; 
     }
     print_tableau (tab_res, n);
+    */
 
+    //Affichage d'une chaîne de caractères tapée à l'écran
+    char chaine[60];
+    printf("Entrez la chaîne de caractères\n");
+    scanf("%s", &chaine);
+    printf("La chaîne que vous venez de taper est : %s\n Est-ce exact ?\n",chaine);
 }
