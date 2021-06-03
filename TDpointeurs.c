b7f567893c52accd4293dd8c30d65dd6ba517990
Louis Marion <marionlouis1@gmail.com>
2021-06-03 10:45:47 +0200

    mylen pointeurs

--- TDpointeurs.c
+++ TDpointeurs.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
 
+int mylen2 (char *s ) {
+    int compteur = 0 ;
+    while (*s++) {
+
+        compteur++ ;
+    }
+    return compteur;
+}
+
 
 int main() {
 
 //Exercice 0
+/*
 printf("Approve moi bogoss");
-
+*/
+//Exercice 2
+char* s = "salut" ;
+printf("%s\n", s);
+printf("La longueur de la chaîne est : %d\n" , mylen2(s));
 }
